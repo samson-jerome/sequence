5a52f3e6079cf6393ae1eb81ea14107c69c57bd9
jerome <samson.jerome@gmail.com>
2019-10-01 00:19:21 +0100

    more refactor

--- src/seq-parser/seqparser.cpp
+++ src/seq-parser/seqparser.cpp
@@ -39,28 +39,16 @@ std::tuple<vector<Collection>, vector<string>> SequenceParser::assemble(vector<s
     // Filter hidden
     // Filter folders, files, symlink and special items
 
-    // Handle negative frames
-    // QRegularExpression re("(?<index>-*\\d+)(?<tail>\\D+)$");
-    // QRegularExpressionMatch match;
-    // string fnames[] = {"aaa11.txt", "bar12.txt", "zoid452berg"};
-    // // regex txt_regex("[a-z]+\\.txt");
-    // regex txt_regex(".*(\\d+?)\\.txt");
-    // for (const auto &fname : fnames) {
-    //     std::cout << fname << ": " << regex_match(fname, txt_regex) << '\n';
+    // TODO probably better to avoid multimap here, see following ref
+    // http://www.yosoygames.com.ar/wp/2014/05/the-sorted-vector-pattern-part-i/
 
-    // }
-
-    // const regex re("(?<index>-*\\d+)(?<tail>\\D+)$"); 
     regex re("(-*\\d+)(\\D*)$"); 
-
     smatch match; 
-  
-    // QMultiHash<QString, int> collectHash;
 
     string head, tail, index, hash;
     set<string> keys;
-    unordered_multimap<string, int> collectHash;
-    typedef unordered_multimap<string, int>::iterator collectIt;
+    unordered_multimap<string, int> collections_ummap;
+    typedef unordered_multimap<string, int>::iterator ummap_it;
     typedef set<string>::iterator keysIt;
 
     for (int i = 0; i < entries.size(); ++i) {
@@ -78,7 +66,7 @@ std::tuple<vector<Collection>, vector<string>> SequenceParser::assemble(vector<s
 
             // Limit size of int, sequence can't have more than ~2B frame
             // as a global range
-            collectHash.insert(make_pair(hash, stoi(index)));
+            collections_ummap.insert(make_pair(hash, stoi(index)));
             if(i < 20){
                 std::cout << (entries.at(i)) << " --> " \
                     << (hash) << " / " \
@@ -96,170 +84,48 @@ std::tuple<vector<Collection>, vector<string>> SequenceParser::assemble(vector<s
         }
     }
 
-    // vector<string> keys = collectHash.uniqueKeys();
-    cout << "Unique keys:" << endl;
-    for (keysIt it=keys.begin(); it!=keys.end(); ++it)
+    cout << "Sequence unique keys:" << endl;
+    for (keysIt key=keys.begin(); key!=keys.end(); ++key)
     {
-        std::cout << ' ' << *it;
+        cout << *key;
         // QStringList splits = it->split('|');
         vector<string> splits;
         string delims = "|";
-        boost::split(splits, *it, boost::is_any_of(delims));
-        cout << "-- splits:" << splits.at(0) << endl;
-        // QList<int> indexes = collectHash.values(*it);
+        boost::split(splits, *key, boost::is_any_of(delims));
+    
+        vector<int> indexes;
+        pair<ummap_it, ummap_it> result = collections_ummap.equal_range(*key);
+
+        // Iterate over the range
+        for (ummap_it it = result.first; it != result.second; it++)
+            cout << "  - " << it->second << endl;
+    
+        // Total Elements in the range
+        int count = distance(result.first, result.second);
+        cout << "Total values " << *key <<" are : " << count << endl;
+
 
         // Identify hash entries with a single index to handle as a remainder instead of collection
-        // if (indexes.count()==1) {
-        //     auto singleFile = splits.at(1) + QString::number(indexes[0]) + splits.at(0);
-        //     remainders.append(singleFile);
-        //     continue;
-        // }
+        if (count==1) {
+            // FIXME bug with zero padding indexes
+            auto singleFile = splits.at(1) + std::to_string(indexes[0]) + splits.at(0);
+            remainders.push_back(singleFile);
+            continue;
+        }
 
-        // // Create the collection for the current hash key
-        // qSort(indexes);
+        // Create the collection for the current hash key
+        sort(indexes.begin(), indexes.end());
+
+        // TOFIX copy constructor or smart pointer or ???
         // Collection currentCollection( 
-        //     splits.at(1).toLocal8Bit().constData(), 
-        //     splits.at(0).toLocal8Bit().constData(), 
-        //     indexes.toVector().toStdVector()
+        //     splits.at(1), 
+        //     splits.at(0), 
+        //     copy(indexes)
         // );
 
         // collections.append(currentCollection);
     
     }
 
-
-    //     // match = re.match(entries.at(i));
-    //     if (match.hasMatch()) {
-    //         // std::cout << "Has match" << std::endl;
-    //         head = entries.at(i).left(match.capturedStart("index"));
-    //         index = match.captured("index");
-    //         tail = match.captured("tail");
-    //         hash = tail + "|" + head;  // convention to store unicity of a sequence
-    
-    //         // Limit size of int, sequence can't have more than ~2B frame
-    //         // as a global range
-    //         collectHash.insert(hash, index.toInt());
-
-    //         // if(i < 20){
-    //         //     std::cout << qPrintable(entries.at(i)) << " --> " \
-    //         //         << qPrintable(hash) << " / " \
-    //         //         << qPrintable(head) << " / " \
-    //         //         << qPrintable(index) << " / " \
-    //         //         << qPrintable(tail) << std::endl;
-    //         // }
-    //         // else{
-    //         //     if(i == 21) std::cout << "..." << std::endl;
-    //         // }
-    //     }
-    //     else {
-    //         remainders.append(entries.at(i));
-    //         // std::cout << qPrintable(entries.at(i)) << " --> remainders" << std::endl;
-    //     }
-    // }
-
-    // QStringList keys = collectHash.uniqueKeys();
-    // for (QStringList::Iterator it = keys.begin(); it != keys.end(); it++)
-    // {
-    //     QStringList splits = it->split('|');
-    //     QList<int> indexes = collectHash.values(*it);
-
-    //     // Identify hash entries with a single index to handle as a remainder instead of collection
-    //     if (indexes.count()==1) {
-    //         auto singleFile = splits.at(1) + QString::number(indexes[0]) + splits.at(0);
-    //         remainders.append(singleFile);
-    //         continue;
-    //     }
-
-    //     // Create the collection for the current hash key
-    //     qSort(indexes);
-    //     Collection currentCollection( 
-    //         splits.at(1).toLocal8Bit().constData(), 
-    //         splits.at(0).toLocal8Bit().constData(), 
-    //         indexes.toVector().toStdVector()
-    //     );
-
-    //     collections.append(currentCollection);
-    // } 
-
-    // return std::make_tuple(collections, remainders);
-    // // return std::make_tuple(collections.toVector().toStdVector(), remainders.toVector().toStdVector());
-
-
     return make_tuple(collections, remainders);
 }
-// void FileParser::assemble(QStringList entries, std::vector<Collection>* collections, std::vector<std::string>* remainders) {
-// }
-
-// std::tuple<QList<Collection>, QStringList> 
-//     FileParser::assemble(QStringList entries) {
-
-//     // Filter hidden
-//     // Filter folders, files, symlink and special items
-
-//     // Handle negative frames
-//     QRegularExpression re("(?<index>-*\\d+)(?<tail>\\D+)$");
-//     QRegularExpressionMatch match;
-
-//     QList<Collection> collections;
-//     QStringList remainders;
-
-//     QMultiHash<QString, int> collectHash;
-//     QString head, tail, index, hash;
-
-//     for (int i = 0; i < entries.size(); ++i) {
-//         match = re.match(entries.at(i));
-//         if (match.hasMatch()) {
-//             // std::cout << "Has match" << std::endl;
-//             head = entries.at(i).left(match.capturedStart("index"));
-//             index = match.captured("index");
-//             tail = match.captured("tail");
-//             hash = tail + "|" + head;  // convention to store unicity of a sequence
-    
-//             // Limit size of int, sequence can't have more than ~2B frame
-//             // as a global range
-//             collectHash.insert(hash, index.toInt());
-
-//             // if(i < 20){
-//             //     std::cout << qPrintable(entries.at(i)) << " --> " \
-//             //         << qPrintable(hash) << " / " \
-//             //         << qPrintable(head) << " / " \
-//             //         << qPrintable(index) << " / " \
-//             //         << qPrintable(tail) << std::endl;
-//             // }
-//             // else{
-//             //     if(i == 21) std::cout << "..." << std::endl;
-//             // }
-//         }
-//         else {
-//             remainders.append(entries.at(i));
-//             // std::cout << qPrintable(entries.at(i)) << " --> remainders" << std::endl;
-//         }
-//     }
-
-//     QStringList keys = collectHash.uniqueKeys();
-//     for (QStringList::Iterator it = keys.begin(); it != keys.end(); it++)
-//     {
-//         QStringList splits = it->split('|');
-//         QList<int> indexes = collectHash.values(*it);
-
-//         // Identify hash entries with a single index to handle as a remainder instead of collection
-//         if (indexes.count()==1) {
-//             auto singleFile = splits.at(1) + QString::number(indexes[0]) + splits.at(0);
-//             remainders.append(singleFile);
-//             continue;
-//         }
-
-//         // Create the collection for the current hash key
-//         qSort(indexes);
-//         Collection currentCollection( 
-//             splits.at(1).toLocal8Bit().constData(), 
-//             splits.at(0).toLocal8Bit().constData(), 
-//             indexes.toVector().toStdVector()
-//         );
-
-//         collections.append(currentCollection);
-//     } 
-
-//     return std::make_tuple(collections, remainders);
-//     // return std::make_tuple(collections.toVector().toStdVector(), remainders.toVector().toStdVector());
-// }
